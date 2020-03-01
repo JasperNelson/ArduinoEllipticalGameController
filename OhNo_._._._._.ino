6fd8e741a931c67951e2579c41ac6cd0bd2d9bf8
JasperNelson <35386705+JasperNelson@users.noreply.github.com>
2020-03-01 13:26:01 -0800

    Update OhNo_._._._._.ino

--- OhNo_._._._._.ino
+++ OhNo_._._._._.ino
@@ -1,20 +1,28 @@
 #include<Keyboard.h>
 
+// Quadrature enccoder setup
 int potnum;
 int val;
-int encoder0PinA = 4;
-int encoder0PinB = 3;
-int encoder0Pos = 0;
-int encoder0PinALast = LOW; // Low voltage value
-int n = LOW;
+const int encoderPinA = 4;
+const int encoderPinB = 3;
+
+int encoderPosition = 0;
+int previousEncoderPinA = LOW; // Low voltage value
+int currentEncoderPinA = LOW;
+
+const int quadratureEncoderRepeats = 5;
+
+// Potentiometer setup
 const int potpin = 2; // digital pin connected to switch output
+
 void setup() {
   Keyboard.begin();
-  pinMode (encoder0PinA, INPUT);
-  pinMode (encoder0PinB, INPUT);
+  pinMode (encoderPinA, INPUT);
+  pinMode (encoderPinB, INPUT);
 }
+
 void loop() {
-  potnum = analogRead(potpin) - 511;  //halfway of potentiometer
+  
   char a = 'a';
   char d = 'd';
   int i=0;
@@ -22,19 +30,30 @@ void loop() {
   while (abort == false) { //insurance loop for the controller
 
     // Rotary encoder handling ("w" and "s").
-    n = digitalRead(encoder0PinA);
-    if (n == HIGH) {
-      if (digitalRead(encoder0PinB) == LOW) {
-        encoder0Pos--;
-        Keyboard.press('s');        
+    
+    currentEncoderPinA = digitalRead(encoderPinA);
+    if (previousEncoderPinA == LOW && currentEncoderPinA == HIGH) {
+      // We've detected a pulse change.  Read the next pin to find the pulse direction.      
+      if (digitalRead(encoderPinB) == LOW) {
+        encoderPosition--;
+        for (int j = 0; j < quadratureEncoderRepeats; ++j) {
+          Keyboard.press('s');        
+        }
+        Keyboard.release('s');
       } else {
-        encoder0Pos++;
-        Keyboard.press('w');          
+        encoderPosition++;
+        for (int j = 0; j < quadratureEncoderRepeats; ++j) {
+          Keyboard.press('w');
+        }
+        Keyboard.release('w');
       }
     }
+    previousEncoderPinA = currentEncoderPinA;
 
     // Potentiometer handling ("a" and "d").
-    potnum = analogRead(potpin) - 511;
+    
+    potnum = analogRead(potpin) - 511;  //halfway of potentiometer
+    
     if (abs(potnum) >= 100 ) {   //if the potentiometer is slid out of the negligible zone.
       if(potnum >= 0) {   //if potentiometer is slid to left than it will trigger the a key.
         Keyboard.press('a');
@@ -45,11 +64,10 @@ void loop() {
       Keyboard.release('a');
       Keyboard.release('d');      
     }
-    encoder0PinALast = n;
-    i++;
-    if (i >= 110) {
-      Keyboard.release('s');
-      Keyboard.release('w');
-    }
+    //    i++;
+    //    if (i >= 110) {
+    //      Keyboard.release('s');
+    //      Keyboard.release('w');
+    //    }
   }
 }
